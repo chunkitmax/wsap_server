e7fb560cc89848a3c6c1398a3d0ce6fa4b9938b7
chunkitmax <chunkitmax1125@gmail.com>
2019-01-27 15:15:37 +0800

    put process event handler inisde main function

--- main.js
+++ main.js
@@ -6,23 +6,23 @@ const Browser = require('./browser')
 const Server = require('./server')
 
 
-let browser = new Browser()
-let server = new Server(browser)
-
 async function main() {
+  let browser = new Browser()
+  let server = new Server(browser)
+
+  process.once('beforeExit', () => {
+    browser.release()
+  })
+  process.once('SIGINT', () => {
+    browser.release()
+  })
+  
   try {
     await browser.start()
   } finally {
     await browser.release()
   }
 }
-
-process.once('beforeExit', () => {
-  browser.release()
-})
-process.once('SIGINT', () => {
-  browser.release()
-})
 
 if (require.main === module) {
   main()
