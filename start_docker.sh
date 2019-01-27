a9dac4192e7961a191f57448b9564c0217cd0199
chunkitmax <chunkitmax1125@gmail.com>
2019-01-27 15:19:08 +0800

    give another optional without VNC

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,9 @@
-docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm selenium/standalone-firefox-debug
+FLAG=$1
+
+if [ ${FLAG,,} = 'debug' ]
+then
+  # debug (w/ VNC)
+  docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm selenium/standalone-firefox-debug
+else
+  docker run -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-firefox
+fi
